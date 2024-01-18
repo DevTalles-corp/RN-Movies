186ef7782f9f344ccc0a89ec602ed2c4dff2e5bd
Fernando Herrera <fernando.herrera85@gmail.com>
2024-01-18 09:48:51 -0500

    Currancy Formatter agregado

---
+++ src/config/helpers/formatter.ts
@@ -0,0 +1,8 @@
+export class Formatter {
+  public static currency(value: number): string {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(value);
+  }
+}
