510bbac4c59c978e63b5e9309a0ec9d6202fdd40
Fernando Herrera <fernando.herrera85@gmail.com>
2024-01-18 10:12:03 -0500

    Add react-native-dotenv plugin to babel.config.js and package.json

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: [ 'module:@react-native/babel-preset' ],
+  plugins: [
+    [
+      "module:react-native-dotenv",
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env'
+      }
+    ]
+
+
+
+    // Reanimated
+  ]
 };
--- src/config/adapters/movieDB.adapter.ts
+++ src/config/adapters/movieDB.adapter.ts
@@ -1,10 +1,12 @@
+import { THE_MOVIE_DB_KEY } from '@env';
 import { AxiosAdapter } from './http/axios.adapter';
 
 
 export const movieDBFetcher = new AxiosAdapter({
   baseUrl: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: '9d6a90c2b95d4a700ee5cb9705c94b0f',
+    // api_key: '9d6a90c2b95d4a700ee5cb9705c94b0f',
+    api_key: THE_MOVIE_DB_KEY ?? 'no-key',
     language: 'es'
   }
 });
